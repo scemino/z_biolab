67d72cf9e17479a9a90232c67d62f04f75052e6e
scemino <scemino74@gmail.com>
2024-08-18 21:54:40 +0200

    Fix wrong particles

--- src/entities/particle.zig
+++ src/entities/particle.zig
@@ -11,23 +11,23 @@ fn init(self: *Entity) void {
     self.base.offset = vec2(0, 0);
     self.base.physics = zi.entity.ENTITY_PHYSICS_LITE;
     self.base.friction = vec2(0.25, 0);
-    self.base.restitution = zi.utils.rand_float(0.5, 0.8);
-    self.base.particle.life_time = zi.utils.rand_float(2.0, 5.0);
-    self.base.particle.fade_time = 6;
+    self.base.restitution = zi.utils.randFloat(0.5, 0.8);
+    self.entity.particle.life_time = zi.utils.randFloat(2.0, 5.0);
+    self.entity.particle.fade_time = 6;
 }
 
 fn update(self: *Entity) void {
-    self.particle.life_time -= engine.tick;
+    self.entity.particle.life_time -= @as(f32, @floatCast(zi.engine.tick));
 
-    if (self.particle.life_time < 0) {
-        engine.killEntity(self);
+    if (self.entity.particle.life_time < 0) {
+        engine.entityKill(self);
         return;
     }
-    self.anim.color.a = std.math.clamp(
-        zi.scale(self.particle.life_time, 0, self.particle.fade_time, 0, 255),
+    self.base.anim.color.setA(std.math.clamp(
+        @min(255, @as(usize, @intFromFloat(@floor(zi.utils.scale(self.entity.particle.life_time, 0.0, self.entity.particle.fade_time, 0.0, 255.0))))),
         0,
         255,
-    );
+    ));
     engine.baseUpdate(self);
 }
 
